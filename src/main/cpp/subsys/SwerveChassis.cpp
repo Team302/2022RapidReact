62833d17a3c9e44ed6ef670fd6bc4a326d8445c0
Nick <nfrisch3@outlook.com>
2022-02-08 20:57:12 -0500

    Auton Testing

--- src/main/cpp/subsys/SwerveChassis.cpp
+++ src/main/cpp/subsys/SwerveChassis.cpp
@@ -557,6 +557,7 @@ void SwerveChassis::ResetPosition
 )
 {
     m_poseEstimator.ResetPosition(pose, angle);
+    SetEncodersToZero();
     auto trans = pose - m_pose;
     m_pose = m_pose + trans;
 
