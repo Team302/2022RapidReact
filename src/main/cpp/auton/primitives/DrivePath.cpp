3fd101dfdc4edbf5f9c93bbde6810ff9dbdc6ea6
Nick <nfrisch3@outlook.com>
2022-02-09 20:57:40 -0500

    2/9 WIP Autons

--- src/main/cpp/auton/primitives/DrivePath.cpp
+++ src/main/cpp/auton/primitives/DrivePath.cpp
@@ -46,9 +46,9 @@ DrivePath::DrivePath() : m_chassis(ChassisFactory::GetChassisFactory()->GetIChas
                          //                 frc2::PIDController{1, 0, 0},
                          //                 frc::ProfiledPIDController<units::radian>{1, 0, 0,
                          //                                                           frc::TrapezoidProfile<units::radian>::Constraints{6.28_rad_per_s, 3.14_rad_per_s / 1_s}}),
-                         m_holoController(frc2::PIDController{0, 0, 0},
-                                          frc2::PIDController{0, 0, 0},
-                                          frc::ProfiledPIDController<units::radian>{0, 0, 0,
+                         m_holoController(frc2::PIDController{0.25, 0, 0},
+                                          frc2::PIDController{0.25, 0, 0},
+                                          frc::ProfiledPIDController<units::radian>{0.25, 0, 0,
                                                                                     frc::TrapezoidProfile<units::radian>::Constraints{0_rad_per_s, 0_rad_per_s / 1_s}}),
                          //max velocity of 1 rotation per second and a max acceleration of 180 degrees per second squared.
                          m_PrevPos(m_chassis.get()->GetPose()),
