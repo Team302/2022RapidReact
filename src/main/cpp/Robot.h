28c402e9f8a708be26913cf78cebc8cee834ad5d
Nick <nfrisch3@outlook.com>
2022-03-05 10:14:35 -0500

    WIP Ball Transfer Commit

--- src/main/cpp/Robot.h
+++ src/main/cpp/Robot.h
@@ -20,6 +20,10 @@
 #include <subsys/Intake.h>
 #include <subsys/interfaces/IChassis.h>
 #include <subsys/Shooter.h>
+#include <subsys/Lift.h>
+#include <states/lift/LiftStateMgr.h>
+#include <subsys/Indexer.h>
+#include <states/indexer/IndexerStates.h>
 
 
 
@@ -58,4 +62,13 @@ class Robot : public frc::TimedRobot
 
   ClimberStateMgr*      m_climberStateMgr;
   Climber*              m_climber;
+
+  IndexerStates*        m_leftIndexerStateMgr;
+  Indexer*              m_leftIndexer;
+
+  IndexerStates*        m_rightIndexerStateMgr;
+  Indexer*              m_rightIndexer;
+
+  LiftStateMgr*         m_liftStateMgr;
+  Lift*                 m_lift;
 };
