272d2c0265e0a48dbd8a1829c0a47e28d04c19a7
Aden25 <loslatterya55@stu.lok12.org>
2022-03-14 18:44:53 -0400

    3/10/22
    Added to motor stall.

--- src/main/cpp/hw/MotorData.cpp
+++ src/main/cpp/hw/MotorData.cpp
@@ -17,11 +17,20 @@
 #include <hw/MotorData.h>
 #include <hw/interfaces/IDragonMotorController.h>
 
-MotorData::MotorData(){}
 
-int MotorData::getStallCurrent(IDragonMotorController::MOTOR_TYPE motorType) const
-{
-    switch (motorType)
+    MotorData* MotorData::m_instance = nullptr;
+    MotorData* MotorData::GetInstance()
+    {	
+        if ( MotorData::m_instance == nullptr )
+	    {
+		    MotorData::m_instance = new MotorData();
+	    }
+	    return MotorData::m_instance;
+       
+    }
+    int MotorData::getStallCurrent(IDragonMotorController::MOTOR_TYPE motorType) const
+    {
+        switch (motorType)
     {
     case IDragonMotorController::FALCON500:
         return stallCurrentValues[IDragonMotorController::FALCON500];
@@ -61,22 +70,186 @@ int MotorData::getStallCurrent(IDragonMotorController::MOTOR_TYPE motorType) con
     default:
         return 0;
     }
-}
-double MotorData::getFreeCurrent(IDragonMotorController::MOTOR_TYPE motorType) const
+    }
+    double MotorData::getFreeCurrent(IDragonMotorController::MOTOR_TYPE motorType) const
+    {
+
+        switch (motorType)
+    {
+    case IDragonMotorController::FALCON500:
+        return freeCurrentValues[IDragonMotorController::FALCON500];
+    case IDragonMotorController::NEOMOTOR:
+        return freeCurrentValues[IDragonMotorController::NEOMOTOR];
+    case IDragonMotorController::NEO500MOTOR:
+        return freeCurrentValues[IDragonMotorController::NEO500MOTOR];
+    case IDragonMotorController::CIMMOTOR:
+        return freeCurrentValues[IDragonMotorController::CIMMOTOR];
+    case IDragonMotorController::MINICIMMOTOR:
+        return freeCurrentValues[IDragonMotorController::MINICIMMOTOR];
+    case IDragonMotorController::BAGMOTOR:
+        return freeCurrentValues[IDragonMotorController::BAGMOTOR];
+    case IDragonMotorController::PRO775:
+        return freeCurrentValues[IDragonMotorController::PRO775];
+    case IDragonMotorController::ANDYMARK9015:
+        return freeCurrentValues[IDragonMotorController::ANDYMARK9015];
+    case IDragonMotorController::ANDYMARKNEVEREST:
+        return freeCurrentValues[IDragonMotorController::ANDYMARKNEVEREST];
+    case IDragonMotorController::ANDYMARKRS775125:
+        return freeCurrentValues[IDragonMotorController::ANDYMARKRS775125];
+    case IDragonMotorController::ANDYMARKREDLINEA:
+        return freeCurrentValues[IDragonMotorController::ANDYMARKREDLINEA];
+    case IDragonMotorController::REVROBOTICSHDHEXMOTOR:
+        return freeCurrentValues[IDragonMotorController::REVROBOTICSHDHEXMOTOR];
+    case IDragonMotorController::BANEBOTSRS77518V:
+        return freeCurrentValues[IDragonMotorController::BANEBOTSRS77518V];
+    case IDragonMotorController::BANEBOTSRS550:
+        return freeCurrentValues[IDragonMotorController::BANEBOTSRS550];
+    case IDragonMotorController::MODERNROBOTICS12VDCMOTOR:
+        return freeCurrentValues[IDragonMotorController::MODERNROBOTICS12VDCMOTOR];
+    case IDragonMotorController::JOHNSONELECTRICALGEARMOTOR:
+        return freeCurrentValues[IDragonMotorController::JOHNSONELECTRICALGEARMOTOR];
+    case IDragonMotorController::TETRIXMAXTORQUENADOMOTOR:
+        return freeCurrentValues[IDragonMotorController::TETRIXMAXTORQUENADOMOTOR];                        
+    default:
+        return 0;
+    }
+
+    }
+    int MotorData::getFreeSpeed(IDragonMotorController::MOTOR_TYPE motorType) const
+    {
+
+     switch (motorType)
+    {
+    case IDragonMotorController::FALCON500:
+        return freeSpeedValues[IDragonMotorController::FALCON500];
+    case IDragonMotorController::NEOMOTOR:
+        return freeSpeedValues[IDragonMotorController::NEOMOTOR];
+    case IDragonMotorController::NEO500MOTOR:
+        return freeSpeedValues[IDragonMotorController::NEO500MOTOR];
+    case IDragonMotorController::CIMMOTOR:
+        return freeSpeedValues[IDragonMotorController::CIMMOTOR];
+    case IDragonMotorController::MINICIMMOTOR:
+        return freeSpeedValues[IDragonMotorController::MINICIMMOTOR];
+    case IDragonMotorController::BAGMOTOR:
+        return freeSpeedValues[IDragonMotorController::BAGMOTOR];
+    case IDragonMotorController::PRO775:
+        return freeSpeedValues[IDragonMotorController::PRO775];
+    case IDragonMotorController::ANDYMARK9015:
+        return freeSpeedValues[IDragonMotorController::ANDYMARK9015];
+    case IDragonMotorController::ANDYMARKNEVEREST:
+        return freeSpeedValues[IDragonMotorController::ANDYMARKNEVEREST];
+    case IDragonMotorController::ANDYMARKRS775125:
+        return freeSpeedValues[IDragonMotorController::ANDYMARKRS775125];
+    case IDragonMotorController::ANDYMARKREDLINEA:
+        return freeSpeedValues[IDragonMotorController::ANDYMARKREDLINEA];
+    case IDragonMotorController::REVROBOTICSHDHEXMOTOR:
+        return freeSpeedValues[IDragonMotorController::REVROBOTICSHDHEXMOTOR];
+    case IDragonMotorController::BANEBOTSRS77518V:
+        return freeSpeedValues[IDragonMotorController::BANEBOTSRS77518V];
+    case IDragonMotorController::BANEBOTSRS550:
+        return freeSpeedValues[IDragonMotorController::BANEBOTSRS550];
+    case IDragonMotorController::MODERNROBOTICS12VDCMOTOR:
+        return freeSpeedValues[IDragonMotorController::MODERNROBOTICS12VDCMOTOR];
+    case IDragonMotorController::JOHNSONELECTRICALGEARMOTOR:
+        return freeSpeedValues[IDragonMotorController::JOHNSONELECTRICALGEARMOTOR];
+    case IDragonMotorController::TETRIXMAXTORQUENADOMOTOR:
+        return freeSpeedValues[IDragonMotorController::TETRIXMAXTORQUENADOMOTOR];                        
+    default:
+        return 0;
+    }
+
+    }
+    int MotorData::getMaximumPower(IDragonMotorController::MOTOR_TYPE motorType) const
+    {
+
+        switch (motorType)
+    {
+    case IDragonMotorController::FALCON500:
+        return maximumPowerValues[IDragonMotorController::FALCON500];
+    case IDragonMotorController::NEOMOTOR:
+        return maximumPowerValues[IDragonMotorController::NEOMOTOR];
+    case IDragonMotorController::NEO500MOTOR:
+        return maximumPowerValues[IDragonMotorController::NEO500MOTOR];
+    case IDragonMotorController::CIMMOTOR:
+        return maximumPowerValues[IDragonMotorController::CIMMOTOR];
+    case IDragonMotorController::MINICIMMOTOR:
+        return maximumPowerValues[IDragonMotorController::MINICIMMOTOR];
+    case IDragonMotorController::BAGMOTOR:
+        return maximumPowerValues[IDragonMotorController::BAGMOTOR];
+    case IDragonMotorController::PRO775:
+        return maximumPowerValues[IDragonMotorController::PRO775];
+    case IDragonMotorController::ANDYMARK9015:
+        return maximumPowerValues[IDragonMotorController::ANDYMARK9015];
+    case IDragonMotorController::ANDYMARKNEVEREST:
+        return maximumPowerValues[IDragonMotorController::ANDYMARKNEVEREST];
+    case IDragonMotorController::ANDYMARKRS775125:
+        return maximumPowerValues[IDragonMotorController::ANDYMARKRS775125];
+    case IDragonMotorController::ANDYMARKREDLINEA:
+        return maximumPowerValues[IDragonMotorController::ANDYMARKREDLINEA];
+    case IDragonMotorController::REVROBOTICSHDHEXMOTOR:
+        return maximumPowerValues[IDragonMotorController::REVROBOTICSHDHEXMOTOR];
+    case IDragonMotorController::BANEBOTSRS77518V:
+        return maximumPowerValues[IDragonMotorController::BANEBOTSRS77518V];
+    case IDragonMotorController::BANEBOTSRS550:
+        return maximumPowerValues[IDragonMotorController::BANEBOTSRS550];
+    case IDragonMotorController::MODERNROBOTICS12VDCMOTOR:
+        return maximumPowerValues[IDragonMotorController::MODERNROBOTICS12VDCMOTOR];
+    case IDragonMotorController::JOHNSONELECTRICALGEARMOTOR:
+        return maximumPowerValues[IDragonMotorController::JOHNSONELECTRICALGEARMOTOR];
+    case IDragonMotorController::TETRIXMAXTORQUENADOMOTOR:
+        return maximumPowerValues[IDragonMotorController::TETRIXMAXTORQUENADOMOTOR];                        
+    default:
+        return 0;
+    }
+
+
+    }
+    double MotorData::getStallTorque(IDragonMotorController::MOTOR_TYPE motorType) const
 {
-
+    switch (motorType)
+    {
+    case IDragonMotorController::FALCON500:
+        return stallTorqueValues[IDragonMotorController::FALCON500];
+    case IDragonMotorController::NEOMOTOR:
+        return stallTorqueValues[IDragonMotorController::NEOMOTOR];
+    case IDragonMotorController::NEO500MOTOR:
+        return stallTorqueValues[IDragonMotorController::NEO500MOTOR];
+    case IDragonMotorController::CIMMOTOR:
+        return stallTorqueValues[IDragonMotorController::CIMMOTOR];
+    case IDragonMotorController::MINICIMMOTOR:
+        return stallTorqueValues[IDragonMotorController::MINICIMMOTOR];
+    case IDragonMotorController::BAGMOTOR:
+        return stallTorqueValues[IDragonMotorController::BAGMOTOR];
+    case IDragonMotorController::PRO775:
+        return stallTorqueValues[IDragonMotorController::PRO775];
+    case IDragonMotorController::ANDYMARK9015:
+        return stallTorqueValues[IDragonMotorController::ANDYMARK9015];
+    case IDragonMotorController::ANDYMARKNEVEREST:
+        return stallTorqueValues[IDragonMotorController::ANDYMARKNEVEREST];
+    case IDragonMotorController::ANDYMARKRS775125:
+        return stallTorqueValues[IDragonMotorController::ANDYMARKRS775125];
+    case IDragonMotorController::ANDYMARKREDLINEA:
+        return stallTorqueValues[IDragonMotorController::ANDYMARKREDLINEA];
+    case IDragonMotorController::REVROBOTICSHDHEXMOTOR:
+        return stallTorqueValues[IDragonMotorController::REVROBOTICSHDHEXMOTOR];
+    case IDragonMotorController::BANEBOTSRS77518V:
+        return stallTorqueValues[IDragonMotorController::BANEBOTSRS77518V];
+    case IDragonMotorController::BANEBOTSRS550:
+        return stallTorqueValues[IDragonMotorController::BANEBOTSRS550];
+    case IDragonMotorController::MODERNROBOTICS12VDCMOTOR:
+        return stallTorqueValues[IDragonMotorController::MODERNROBOTICS12VDCMOTOR];
+    case IDragonMotorController::JOHNSONELECTRICALGEARMOTOR:
+        return stallTorqueValues[IDragonMotorController::JOHNSONELECTRICALGEARMOTOR];
+    case IDragonMotorController::TETRIXMAXTORQUENADOMOTOR:
+        return stallTorqueValues[IDragonMotorController::TETRIXMAXTORQUENADOMOTOR];                        
+    default:
+        return 0;
+    }
 }
-int MotorData::getFreeSpeed(IDragonMotorController::MOTOR_TYPE motorType) const
-{
 
-}
-int MotorData::getMaximumPower(IDragonMotorController::MOTOR_TYPE motorType) const
-{
+    bool MotorData::checkIfStall(std::shared_ptr<IDragonMotorController> motor)
+    {
 
-}
-double MotorData::getStallTorque(IDragonMotorController::MOTOR_TYPE motorType) const
-{
-
-}
-
-
+         auto motorType = motor.get()->GetMotorType();
+        return motor.get()->GetCurrent() >= getStallCurrent(motorType)*0.90;
+    }
--- src/main/cpp/hw/MotorData.h
+++ src/main/cpp/hw/MotorData.h
@@ -26,10 +26,11 @@
 
 class MotorData
 {
+
+    
+   
     public:
 
-        MotorData() = default;
-        ~MotorData() = default;
 
     
     int getStallCurrent(IDragonMotorController::MOTOR_TYPE motorType) const;
@@ -37,10 +38,13 @@ class MotorData
     int getFreeSpeed(IDragonMotorController::MOTOR_TYPE motorType) const;
     int getMaximumPower(IDragonMotorController::MOTOR_TYPE motorType) const;
     double getStallTorque(IDragonMotorController::MOTOR_TYPE motorType) const;
-
+    bool checkIfStall(std::shared_ptr<IDragonMotorController> motor);
+    static MotorData* GetInstance();
 
     private:
-    
+     MotorData() = default;
+    ~MotorData() = default;
+
     const int stallCurrentValues[18] = {257, //Falcon 500
                                         166, //NEO Motor
                                         111, //NEO 550 Motor
@@ -59,6 +63,82 @@ class MotorData
                                         21,  //Johnson Electric Gear Motor
                                         9,   //TETRIX MAX TorqueNADO Motor
                                         0};  //No motor
+    
+    const double freeCurrentValues[18] = {1.5, //Falcon 500
+                                        1.3, //NEO Motor
+                                        1.1, //NEO 550 Motor
+                                        2.7, //CIM Motor
+                                        3,  //Mini CIM Motor
+                                        1.8,  //BAG Motor
+                                        0.7, //775pro Motor
+                                        3.7,  //AndyMark 9015
+                                        0.4,  //AndyMark NeveRest
+                                        1.6,  //AndyMark RS775-125
+                                        2.6, //AndyMark Redline A
+                                        0.3,  //REV Robotgics HD Hex Motor
+                                        2.7,  //BaneBots RS-775 18V
+                                        0.4,  //BaneBots RS-550
+                                        0.3,  //Modern Robotics 12VDC Motor
+                                        0.9,  //Johnson Electric Gear Motor
+                                        0.2,   //TETRIX MAX TorqueNADO Motor
+                                        0};  //No motor
+    
+    const int maximumPowerValues[18] = {783, //Falcon 500
+                                        516, //NEO Motor
+                                        332, //NEO 550 Motor
+                                        337, //CIM Motor
+                                        215,  //Mini CIM Motor
+                                        149,  //BAG Motor
+                                        347, //775pro Motor
+                                        134,  //AndyMark 9015
+                                        25,  //AndyMark NeveRest
+                                        43,  //AndyMark RS775-125
+                                        327, //AndyMark Redline A
+                                        28,  //REV Robotgics HD Hex Motor
+                                        246,  //BaneBots RS-775 18V
+                                        190,  //BaneBots RS-550
+                                        29,  //Modern Robotics 12VDC Motor
+                                        45,  //Johnson Electric Gear Motor
+                                        26,   //TETRIX MAX TorqueNADO Motor
+                                        0};  //No motor
 
+    const double stallTorqueValues[18] = {4.69, //Falcon 500
+                                        3.36, //NEO Motor
+                                        1.08, //NEO 550 Motor
+                                        2.41, //CIM Motor
+                                        1.41,  //Mini CIM Motor
+                                        0.43,  //BAG Motor
+                                        0.71, //775pro Motor
+                                        0.36,  //AndyMark 9015
+                                        0.17,  //AndyMark NeveRest
+                                        0.28,  //AndyMark RS775-125
+                                        0.64, //AndyMark Redline A
+                                        0.182,  //REV Robotgics HD Hex Motor
+                                        0.72,  //BaneBots RS-775 18V
+                                        0.38,  //BaneBots RS-550
+                                        0.19,  //Modern Robotics 12VDC Motor
+                                        4.09,  //Johnson Electric Gear Motor
+                                        0.17,   //TETRIX MAX TorqueNADO Motor
+                                        0};  //No motor
+    
+    const int freeSpeedValues[18] = {6380, //Falcon 500
+                                        5880, //NEO Motor
+                                        11710, //NEO 550 Motor
+                                        5330, //CIM Motor
+                                        5840,  //Mini CIM Motor
+                                        13180,  //BAG Motor
+                                        18730, //775pro Motor
+                                        14270,  //AndyMark 9015
+                                        5480,  //AndyMark NeveRest
+                                        5800,  //AndyMark RS775-125
+                                        19500, //AndyMark Redline A
+                                        5960,  //REV Robotgics HD Hex Motor
+                                        13050,  //BaneBots RS-775 18V
+                                        19000,  //BaneBots RS-550
+                                        5900,  //Modern Robotics 12VDC Motor
+                                        420,  //Johnson Electric Gear Motor
+                                        5920,   //TETRIX MAX TorqueNADO Motor
+                                        0};  //No motor
 
+    static MotorData* m_instance;
 };
