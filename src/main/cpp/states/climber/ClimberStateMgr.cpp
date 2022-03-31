a4632ab60e558a027228d71816d8dd76137c4f6e
Joe Witcpalek <joe.witcpalek@gmail.com>
2022-03-31 16:46:07 -0400

    Update States
    Add additional states
    Add analog inputs

--- src/main/cpp/states/climber/ClimberStateMgr.cpp
+++ src/main/cpp/states/climber/ClimberStateMgr.cpp
@@ -73,9 +73,8 @@ ClimberStateMgr::ClimberStateMgr() : m_climber(MechanismFactory::GetMechanismFac
     
     // initialize the xml string to state map
     map<string, StateStruc> stateMap;
-    stateMap[m_climberOffXmlString] = m_offState;
-    stateMap[m_climberManualXmlString] = m_manualState;
     stateMap[m_climberStartingXmlString] = m_startingState;
+    stateMap[m_climberManualXmlString] = m_manualState;
     stateMap[m_climberPrepMidBarXmlString] = m_prepMidBarState;
     stateMap[m_climberClimbMidBarXmlString] = m_climbMidBarState;
     stateMap[m_climberFrontHookPrepXmlString] = m_frontHookprepNextBarState;
@@ -113,18 +112,9 @@ void ClimberStateMgr::CheckForStateTransition()
 
         if (isClimbMode)
         {
-
-            auto isPrepMidbar = controller != nullptr ? controller->IsButtonPressed(TeleopControl::FUNCTION_IDENTIFIER::PREP_MIDBAR_CLIMB) : false;
             auto isAutoClimb = controller != nullptr ? controller->IsButtonPressed(TeleopControl::FUNCTION_IDENTIFIER::CLIMB_AUTO) : false;
-
-            if (isPrepMidbar)
+            if (isAutoClimb)
             {
-                targetState = CLIMBER_STATE::PREP_MID_BAR;
-                m_prevState = targetState;
-            }
-            else if (isAutoClimb)
-            {
-                m_wasAutoClimb = true;
                 auto currentStatePtr = GetCurrentStatePtr();
                 if (currentStatePtr != nullptr)
                 {
@@ -133,12 +123,7 @@ void ClimberStateMgr::CheckForStateTransition()
                     {
                         targetState = static_cast<CLIMBER_STATE>(static_cast<int>(currentState)+1);
                     }
-                    m_prevState = targetState;
                 }
-            }
-            else if (m_wasAutoClimb)
-            {
-                targetState = m_prevState;
             }
             else
             {
@@ -147,7 +132,7 @@ void ClimberStateMgr::CheckForStateTransition()
         }
         else
         {
-            targetState = CLIMBER_STATE::OFF;
+            targetState = CLIMBER_STATE::STARTING_CONFIG;
         }
 
         Logger::GetLogger()->ToNtTable(m_nt, string("state"), targetState);
